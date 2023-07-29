d286f1ff5357b04eda8755174c9b37208b3e9a97
pkshitij291 <pkshitij291@gmail.com>
2023-07-29 14:13:54 +0530

    main

---
+++ src/Components/Results/Results.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { CiSearch } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+import { faker } from '@faker-js/faker';
+import Star from './Star.png';
+import filledheart from './filledheart.png';
+import unfilledheart from './unfilledheart.png';
+
+import './Results.css'
+
+
+const Results = () => {
+
+  // const randomNumber = Math.random();
+
+  
+
+  function getRandomNumber(min:number,max:number):number{
+    return Math.floor(Math.random()* (max-min)+ min);
+  }
+
+  const productData = Array.from({ length: 8 }, () => ({
+    name: faker.commerce.productName(),
+    imageUrl: faker.image.imageUrl(239, 325, 'clothing'),
+    
+  }));
+
+  const[isExpanded, setIsExpanded] = useState(true);
+  const handleToggle = ()=>{
+    setIsExpanded(!isExpanded)
+  }
+  const [favorites,setFavorites] = useState(new Array(productData.length).fill(false));
+  const toggleFavorite =(index:number)=>{
+    const updatedFavorites = [...favorites];
+    updatedFavorites[index] = !updatedFavorites[index];
+    setFavorites(updatedFavorites);
+  }
+  return (
+    <div>
+      <div>
+      <div className='result'>
+            <input className='resultsearch' type='name' placeholder='Search'/>
+
+            <CiSearch className='searchicons'/>
+      </div>
+      <div className='searchresult'>
+        <p>Search Results</p>
+        <div style={{position:'relative'}}>
+          <div className='brandname' onClick={handleToggle}>
+            <p>BRAND</p>
+            <IoIosArrowDown className={`downarrow ${isExpanded ? 'expanded' : ''}`}/>
+          </div>
+          {isExpanded && (
+            <div className='brand-content'>
+              <label className='label'>
+                <input className='checkbox' type='checkbox'/>
+                <div>H&M</div>
+              </label>
+              <label className='label'>
+                <input className='checkbox' type='checkbox'/>
+                <div>Mango</div>
+              </label>
+              
+            </div>
+          )}
+          
+        </div>
+        </div>
+      </div>
+      
+      
+      <div className='productlist'>
+       
+          <div>
+            {productData.map((product, index) => (
+            <div key={index}>
+              <div style={{position:'relative'}}>
+              <img style={{borderRadius: 5}} src={product.imageUrl} alt={`Product ${index + 1}`} />
+              <div className='hearticon' onClick={()=>toggleFavorite(index)}><img src={favorites[index] ? filledheart: unfilledheart} alt='heart'/></div>
+
+              </div>
+              <p style={{marginBottom:-10,fontWeight:400, fontSize:20, position:'relative'}}>{product.name}</p>
+              <div style={{display:'flex',gap: 20,position:'absolute',}}>
+              <p style={{textDecorationLine: 'line-through', color:'rgba(0, 0, 0, 0.40)', fontSize:20, fontWeight:300}}>Rs{getRandomNumber(100,999)}</p>
+              <p style={{color: 'blue', fontWeight:600, fontSize:20,}}>Rs {getRandomNumber(100,999)}</p>
+
+              </div>
+              <div className='star'>
+                <img src={Star} alt='/' />
+                <p>(210)</p>
+              </div>
+            </div>
+              ))}
+          </div>
+          
+      </div>
+    </div>
+  )
+}
+
+export default Results
---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,78 @@
+import React, { useState, ChangeEvent } from 'react'
+import './Search.css';
+import { CiSearch } from "react-icons/ci";
+// import faker from 'faker';
+import { faker } from '@faker-js/faker';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Search = () => {
+
+  const productData = Array.from({ length: 5 }, () => ({
+    name: faker.commerce.productName(),
+    imageUrl: faker.image.imageUrl(130, 170, 'clothing'),
+  }));
+
+  const[input,Setinput] = useState('')
+  const[showSuggestions, setShowSuggestions] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = ()=>{
+    setShowSuggestions(!showSuggestions)
+  }
+  
+
+  const toggle = (e:ChangeEvent<HTMLInputElement>)=> {
+    const value = e.target.value;
+    Setinput(value)
+  }
+  const handleSearch = ()=>{
+    navigate('/results');
+  }
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      navigate('/results');
+    }
+  };
+  return (
+    <div className='search'>
+        <div className='home'>
+            <input className='searchbox' type='name' value={input} placeholder='Search' onChange={toggle} onClick={handleClick} onKeyDown={handleEnter}/>
+
+            <CiSearch className='searchicon' onClick={handleSearch}/>
+        </div>
+        <div>
+          {showSuggestions && <div className={`suggestion ${showSuggestions?'show' : ''}`}>
+          <p className='suggesthead'>Latest Trends</p>
+          <div className='product-container'>
+          {productData.map((product, index) => (
+          <div key={index}>
+            <img className='images' src={product.imageUrl} alt={`Product ${index + 1}`} />
+            <p className='productname'>{product.name}</p>
+          </div>
+            ))}
+          </div>
+            <div className='popsuggest'>
+              <p>Popular suggestions</p>
+              <ul className='suggested list'>
+                <li>Stripped</li>
+                <li>Satin Shirts</li>
+                <li>Denim Jumpsuit</li>
+                <li>Solid tshirts</li>
+                <li>Leather dresses</li>
+              </ul>
+            </div>
+          </div>}
+            
+          
+            
+
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Search;
